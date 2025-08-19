d27b3fa46bcde65835d82cc48d91b895026dde3c
riekhesh <riekhesh1738@gmail.com>
2025-08-19 20:42:56 +0800

    UI polish: footer links/icons, force light mode, mobile bg fix; contact page email + socials

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="color-scheme" content="light" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
           rel="stylesheet"
